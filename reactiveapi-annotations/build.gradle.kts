6cf3c25a2852d78cb652700d39eaf10135fed534
CKATEPTb <ckateptb@vivaldi.net>
2026-04-08 16:02:01 +0200

    Add sources and javadoc jar tasks and update publishing configuration

--- reactiveapi-annotations/build.gradle.kts
+++ reactiveapi-annotations/build.gradle.kts
@@ -12,3 +12,47 @@ publishing {
         }
     }
 }
+
+tasks {
+    register<Jar>("sourcesJar") {
+        archiveClassifier.set("sources")
+        from(sourceSets.main.get().allSource)
+    }
+    register<Jar>("javadocJar") {
+        archiveClassifier.set("javadoc")
+        from(javadoc)
+    }
+    javadoc {
+        options.encoding = "UTF-8"
+        options.memberLevel = JavadocMemberLevel.PUBLIC
+        isFailOnError = false
+    }
+    withType<JavaCompile> {
+        options.encoding = Charsets.UTF_8.name()
+        options.release.set(17)
+    }
+    build {
+        dependsOn("sourcesJar", "javadocJar")
+    }
+    jar {
+        enabled = true
+    }
+}
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            from(components["java"])
+            artifactId = "reactiveapi-annotations"
+        }
+    }
+    repositories {
+        maven {
+            name = "BillmarsSoft"
+            url = uri("https://repo.billmarssoft.com/releases/")
+            credentials {
+                username = System.getenv("REPOSITORY_USERNAME")
+                password = System.getenv("REPOSITORY_PASSWORD")
+            }
+        }
+    }
+}
